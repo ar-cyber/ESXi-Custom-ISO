93d33658389c69693fccad43b07b7e3ab5fc991f
Andrew R <25roband@cbc.sa.edu.au>
2026-01-11 18:10:54 +1030

    Update esxi7.ps1

--- esxi7.ps1
+++ esxi7.ps1
@@ -63,7 +63,7 @@ switch ($choice) {
         if (!(Test-Path $manualUpdate1)){Invoke-WebRequest -Uri $manualUpdateUrl1 -OutFile $($manualUpdate1)}
         Add-EsxSoftwareDepot $manualUpdate1
         Start-Sleep 2
-        $imageProfiles = Get-EsxImageProfile | Where-Object { $_.Name -like "ESXi-$baseESXiVer*-standard*" } | Sort-Object -Property CreationTime -Descending
+        $imageProfiles = Get-EsxImageProfile | Where-Object { $_.Name -like "ESXi-$baseESXiVer*" } | Sort-Object -Property CreationTime -Descending
     }
     
     
@@ -73,7 +73,7 @@ switch ($choice) {
         # Retrieve available image profiles from VMware
         Add-EsxSoftwareDepot https://dl.broadcom.com/$TOKEN/PROD/COMP/ESX_HOST/main/vmw-depot-index.xml
         Start-Sleep 2
-        $imageProfiles = Get-EsxImageProfile | Where-Object { $_.Name -like "ESXi-$baseESXiVer*-standard*" } | Sort-Object -Property CreationTime -Descending
+        $imageProfiles = Get-EsxImageProfile | Where-Object { $_.Name -like "ESXi-$baseESXiVer*" } | Sort-Object -Property CreationTime -Descending
         echo ""
         echo "ESXi-7.0U3s-24585291-standard.zip was the last public download" 
     }
